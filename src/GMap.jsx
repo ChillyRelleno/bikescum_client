0be0f2c31e698f881d6be4588c44bc5e52f1e86d
Phillip Daw <phillipdaw@yahoo.com>
2017-09-21 01:03:32 +0000

    map is showing, marker is not

--- src/GMap.jsx
+++ src/GMap.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import GoogleMapReact from 'google-map-react';
+
 //import d3 from 'd3';
 //import reactD3Map from 'react-d3-map';
-import reactMaps from 'react-google-maps';
+//import reactMaps from 'react-google-maps';
+/*import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  KmlLayer,
+} from "react-google-maps";*/
 
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class GMap extends React.Component {
   //var title = "GPX AQI Fire Map";
   constructor(props) {
     super(props);
-//    this.map_id = props.map_id;//"#mapd3";
-  }
+    this.center = props.center;    
+  //    this.map_id = props.map_id;//"#mapd3";
+  }//constructor
+
+
 
   componentDidMount() {
 
@@ -20,12 +34,26 @@ class GMap extends React.Component {
   }
 
   render() {
+////[59.838043, 30.337157]}
+
     return (
-      <div>
-        <center><h1>{this.props.title}</h1></center>
-        <div id={this.props.map_id} style={{width:"100%"}, {height:"400px"}}></div>
-      </div>
-    )
+      <GoogleMapReact 
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+        bootstrapURLKeys={{
+          key:"AIzaSyBIzV5zhdi5cuH6Qvd5w2k_ddsnKLuts_c"
+        }}
+        style={{height:"400px"}}
+        center={this.center}
+        zoom={8}
+      >
+      <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text={'Kreyser Avrora'}
+        />
+      </GoogleMapReact>
+    );
   }
 
   componentWillUnmount() {
@@ -41,3 +69,4 @@ GMap.propTypes = {
 };
 
 export default GMap;
+
--- src/app.jsx
+++ src/app.jsx
@@ -8,7 +8,11 @@ export default class App extends React.Component {
         <h1>It Works!</h1>
         <p>This React project just works including <span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
-        <GMap />
+        <GMap 
+           style={{height:"400px"}} 
+
+           center={[45.5231,-122.6765]}
+        />
       </div>
     )
   }
