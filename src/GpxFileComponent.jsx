0453ec48ea01063378fee137243754882bcbf3dc
Phillip Daw <phillipdaw@yahoo.com>
2017-11-21 23:58:11 +0000

    refactored so features style theemselves on both url and file uploads

--- src/GpxFileComponent.jsx
+++ src/GpxFileComponent.jsx
@@ -19,7 +19,6 @@ class GpxFileComponent extends React.Component {
   }//constructor
 
   loadGPXUrlIntoGoogleMap = ( url) => {
-    this.props.map.data.addListener('addfeature', this.setFeatureStyle.bind(this));
     fetch(url)
       .then(response => response.text())
       .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
@@ -36,6 +35,9 @@ class GpxFileComponent extends React.Component {
     if (xml !== null) {
       //Setup parser
       var parser = new GPXParser(xml, this.props.map);
+
+      //Set up event so features can color themselves
+      this.props.map.data.addListener('addfeature', this.setFeatureStyle.bind(this));
 
       //prepare for draw, change state
       var fileBoundingBox = parser.centerAndZoom(xml);
@@ -62,7 +64,7 @@ class GpxFileComponent extends React.Component {
 		fillColor: feature.getProperty('color'),
 		fillOpacity: feature.getProperty('opacity')})//0.9});
   }
-  handleChange = function(e, results) {
+  handleFile = function(e, results) {
     console.log('OnChange Fired, results = ');
     console.log(results);
     results.forEach(result => {
@@ -71,7 +73,7 @@ class GpxFileComponent extends React.Component {
     });
     //Plot gpx path
     this.loadGPXFileIntoGoogleMap(results);
-  }//handleChange
+  }//handleFile
 
   useTestData = function(e) {
     console.log('using test data from server');
@@ -91,19 +93,26 @@ class GpxFileComponent extends React.Component {
 			map = {this.props.map}/> 
 		      <FirePerimeterComponent boundingBox = {this.state.boundingBox}
 			google = {this.props.google}
-			map = {this.props.map} /></div>
+			map = {this.props.map} />
+ 		      <form style={floatStyle}>
+     		        <FileReaderInput as="text" id="gpx-file-input"
+            		  onChange={this.handleFile.bind(this)}>
+            		  <button type="button">Select a file!</button>
+          		</FileReaderInput>
+          	        <button type="button" onClick={this.useTestData.bind(this)}>Use demo data</button>
+                      </form>
+</div>
 			);
     }
     else {
-      var floatStyle = {
-        position: 'fixed', float: 'right'
+      var floatStyle = {        position: 'fixed', float: 'right'
         //position: 'absolute', top: '10px', left: '10px', zIndex: 99
       };
 
       selectFile = (
         <form style={floatStyle}>
           <FileReaderInput as="text" id="gpx-file-input"
-	      onChange={this.handleChange.bind(this)}>
+	      onChange={this.handleFile.bind(this)}>
             <button type="button">Select a file!</button>
           </FileReaderInput>
           <button type="button" onClick={this.useTestData.bind(this)}>Use demo data</button>
